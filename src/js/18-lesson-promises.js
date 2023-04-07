bc28b4128d7df4dbd42cd0076d6854132cd52f20
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-04-07 22:52:40 +0300

    add some changes

--- src/js/18-lesson-promises.js
+++ src/js/18-lesson-promises.js
@@ -64,102 +64,104 @@ document
 
 //! ====
 
-setTimeout(function timeout() {
-  console.log('A');
-}, 0);
+// setTimeout(function timeout() {
+//   console.log('A');
+// }, 0);
 
-let p = new Promise(function (resolve, reject) {
-  console.log('B');
-  resolve();
-});
+// let p = new Promise(function (resolve, reject) {
+//   console.log('B');
+//   resolve();
+// });
 
-p.then(function () {
-  console.log('C');
-});
+// p.then(function () {
+//   console.log('C');
+// });
 
-console.log('D');
+// console.log('D');
 
 //! =====
 
-// console.log("Request data...");
+// console.log('Request data...');
 
 // setTimeout(() => {
-//   console.log("Preparing data...");
+//   console.log('Preparing data...');
 //   const user = {
 //     id: 0,
-//     name: "Oleg",
+//     name: 'Oleg',
 //     age: 36,
 //     status: true,
 //   };
 //   setTimeout(() => {
 //     user.status = false;
-//     console.log("Data received: ", user);
+//     console.log('Data received: ', user);
 //   }, 1000);
 // }, 2000);
 
 //!====
 
-// console.log("Request data...");
+// console.log('Request data...');
 
 // const promise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
-//     console.log("Preparing data...");
+//     console.log('Preparing data...');
 //     const user = {
 //       // id: 0,
-//       name: "Oleg",
+//       name: 'Oleg',
 //       age: 36,
 //       status: true,
 //     };
-//     if (user.id === undefined) reject(new Error("No id!"));
+//     if (user.id === undefined) reject(new Error('No id!'));
 //     resolve(user);
 //   }, 2000);
 // });
 
 // promise
-//   .then((data) => {
+//   .then(data => {
 //     setTimeout(() => {
 //       data.status = false;
-//       console.log("Data received: ", data);
+//       console.log('Data received: ', data);
 //     }, 1000);
 //   })
-//   .catch((err) => {
-//     console.error(err.message);
+//   .catch(error => {
+//     console.error(error.message);
 //   });
 
 //! =====
 
-// console.log("Request data...");
+// console.log('Request data...');
 
 // const promise = new Promise((resolve, reject) => {
 //   setTimeout(() => {
-//     console.log("Preparing data...");
+//     console.log('Preparing data...');
 //     const user = {
 //       id: 0,
-//       name: "Oleg",
+//       name: 'Oleg',
 //       age: 36,
 //       status: true,
 //     };
-//     if (user.id === undefined) reject(new Error("No id!"));
+//     if (user.id === undefined) reject(new Error('No id!'));
 //     resolve(user);
 //   }, 2000);
 // });
 
 // promise
-//   .then((data) => {
-//     return new Promise((resolve) => {
+//   .then(data => {
+//     return new Promise(res => {
 //       data.status = false;
-//       resolve(data);
+//       res(data);
 //     });
 //   })
-//   .then((data) => console.log(data))
-//   .catch((err) => {
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(err => {
 //     console.error(err.message);
 //   })
 //   .finally(() => {
-//     console.log("THE END!");
+//     console.log('THE END!');
 //   });
 
-//! =====
+// !========
 
 const sleep = ms =>
   new Promise((res, rej) =>
@@ -169,40 +171,48 @@ const sleep = ms =>
     }, ms),
   );
 
-// sleep(2000).then(() => console.log("проміс виконався через 2с"));
-// sleep(1000).then(() => console.log("проміс виконався через 1с"));
-
-// Promise.all([sleep(2000), sleep(3000), sleep(1000)])
-//   .then((data) => {
-//     console.log("all promises");
-//     console.log(data);
-//   })
-//   .catch((err) => {
+// sleep(2000)
+//   .then(() => console.log('проміс виконався через 2с'))
+//   .catch(err => {
+//     console.error(err);
+//   });
+// sleep(3000)
+//   .then(() => console.log('проміс виконався через 3с'))
+//   .catch(err => {
 //     console.error(err);
 //   });
 
-// Promise.race([sleep(2000), sleep(3000), sleep(1000)])
-//   .then((data) => {
-//     console.log("race promises");
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+Promise.all([sleep(2000), sleep(3000), sleep(1000)])
+  .then(data => {
+    console.log('all promises');
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+Promise.race([sleep(2000), sleep(3000), sleep(1000)])
+  .then(data => {
+    console.log('race promises');
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 //!====
 
-// console.log("start");
-// const promise1 = Promise.resolve().then(() => {
-//   console.log("promise1");
-//   setTimeout(() => {
-//     console.log("timer2");
-//   }, 0);
-// });
-// const timer1 = setTimeout(() => {
-//   console.log("timer1");
-//   Promise.resolve().then(() => {
-//     console.log("promise2");
-//   });
-// }, 0);
-// console.log("end");
+console.log('start');
+const promise1 = Promise.resolve().then(() => {
+  console.log('promise1');
+  setTimeout(() => {
+    console.log('timer2');
+  }, 0);
+});
+const timer1 = setTimeout(() => {
+  console.log('timer1');
+  Promise.resolve().then(() => {
+    console.log('promise2');
+  });
+}, 0);
+console.log('end');
